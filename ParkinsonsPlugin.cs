58d537b73a383c842eaa417b625e9c45314a5af8
LavaDesu <me@lava.moe>
2020-10-14 10:52:22 +0700

    ​

---
+++ ParkinsonsPlugin.cs
@@ -0,0 +1,34 @@
+﻿using OpenTabletDriver.Plugin;
+using OpenTabletDriver.Plugin.Attributes;
+using OpenTabletDriver.Plugin.Tablet;
+using System;
+using System.Numerics;
+
+namespace Parkinsons
+{
+    [PluginName("Parkinsons")]
+    public class ParkinsonsPlugin : IFilter
+    {
+        private static readonly Random seed = new Random();
+
+        public Vector2 Filter(Vector2 point)
+        {
+            point.X += seed.Next(intensity) - offset;
+            point.Y += seed.Next(intensity) - offset;
+            return point;
+        }
+
+        public FilterStage FilterStage => FilterStage.PostTranspose;
+        
+        private int intensity;
+        private int offset;
+        [Property("Intensity")]
+        public int Intensity {
+            get => intensity;
+            set {
+                intensity = value;
+                offset = value / 2;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+https://github.com/LavaDesu/Parkinsons/releases/download/0.0.0/Parkinsons.dll
+
+
+```sh
+dotnet build -c Release -f net5.0 --output "./out"
+```
